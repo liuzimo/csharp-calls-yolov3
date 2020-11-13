5fe73de42ce373ffe360cd9238d835dec9b3c89b
千秋 <919825501@qq.com>
2020-11-13 09:48:18 +0800

    初始化

---
+++ ObjectDetectionGui/Detect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Models;
+using ObjectDetectionGui.Views;
+using DeepLearning;
+
+namespace ObjectDetectionGui
+{
+    public class Detect
+    {
+        static void Main(string[] args)
+        {
+            WrapperModel model = WrapperModel.Instance;
+            if (model.IsDetectionEnable)
+            {
+                BrowserView browserview = new BrowserView();
+                //检测的图片路径
+                browserview.ImagePath = "1.jpg";
+                //检测
+                browserview.Detect();
+                //检测到的类别名称
+                foreach (DetectedItemInfo item in browserview.items)
+                {
+                    var label = item.Type;
+                    Console.WriteLine(label);
+                }
+            }
+        }
+    }
+}
---
+++ ObjectDetectionGui/Views/BrowserView.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Models;
+using DeepLearning;
+using ObjectDetectionGui.Base;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace ObjectDetectionGui.Views
+{
+    public partial class BrowserView
+    {
+
+        #region Fields
+
+        private ResultViewModel m_ResultViewModel = ResultViewModel.Instance;
+        public List<DetectedItemInfo> DetectedItemInfoList { get; set; }
+
+        #endregion
+
+        #region Initializing
+
+        public BrowserView()
+        {
+        }
+
+
+        #endregion
+
+        #region Commands
+
+
+
+        public void Detect()
+        {
+            if (ImagePath !=null)
+            {
+                WrapperModel model = WrapperModel.Instance;
+                model.FilePath = ImagePath;
+                items = model.Detect();
+
+                SetDetectedItemsToBindableItems();
+            }
+            else
+            {
+            }
+        }
+
+        #endregion
+        
+        #region Utilitis
+
+        private void SetDetectedItemsToBindableItems()
+        {
+            m_ResultViewModel.ResultModelList.Clear();
+            foreach (DetectedItemInfo item in items)
+            {
+                ResultInfoModel resultModel = new ResultInfoModel();
+                resultModel.DetectionLabel = item.Type;
+                resultModel.Confidence = item.Confidence;
+                resultModel.DetectedBoxLeftTopX = item.X;
+                resultModel.DetectedBoxLeftTopY = item.Y;
+                resultModel.DetectedBoxWidth = item.Width;
+                resultModel.DetectedBoxHeight = item.Height;
+                m_ResultViewModel.ResultModelList.Add(resultModel);
+            }
+            DrawBoundingBox();
+        }
+        #endregion
+        #region Drawing BoundingBox
+
+        public void DrawBoundingBox()
+        {
+            Image img = Image.FromFile(ImagePath);//建立Image对象
+            Graphics g = Graphics.FromImage(img);//创建Graphics对象
+            Pen pen;
+            Font font = new Font("Arial", 16);
+            foreach (DetectedItemInfo item in items)
+            {
+                pen = new Pen(Brushes.Blue, 2);
+                g.DrawRectangle( pen, new Rectangle(item.X, item.Y, item.Width, item.Height)); 
+                g.DrawString(item.Type, font, Brushes.Red, item.X, item.Y);
+                //ImageFormat iformat=new ImageFormat(new Guid(Bmp));
+            }
+            img.Save(@"image.png");
+            
+        }
+
+        #endregion
+
+        #region Changed Value
+
+
+        #endregion
+
+        public string ImagePath { get; set; }
+        public List<DetectedItemInfo> items { get; set; }
+    }
+}
